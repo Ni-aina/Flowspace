326ae49f524e3448f83f8cb8746669bc8e60babf
ni-aina <niaina.dev.soft@gmail.com>
2026-04-21 10:21:07 +0300

    Features/add menu

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -7,15 +7,17 @@ import {
 } from "@/components/ui/sidebar"
 import SignOut from "./sign-out"
 import { Account } from "./workspaces/account"
+import Menu from "./workspaces/menu"
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader />
       <SidebarContent className="px-2">
-        <SidebarGroup />
-          <Account/>
-        <SidebarGroup />
+        <SidebarGroup className="flex flex-col space-y-2">
+          <Account />
+          <Menu />
+        </SidebarGroup>
       </SidebarContent>
       <SidebarFooter className="flex justify-end">
         <SignOut />
