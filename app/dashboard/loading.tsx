d4a61fe874faceabcbc47a752cac6bc20e0d5d9e
ni-aina <niaina.dev.soft@gmail.com>
2026-05-11 16:40:21 +0300

    Issues/Loading

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -2,7 +2,7 @@ export default function DashboardLoading() {
   return (
     <div className="grid place-items-center w-full h-[85vh]">
       <div 
-        className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary/40"
+        className="animate-spin rounded-full h-12 w-12 border-b-2 border-neutral-800"
       />
     </div>
   )
