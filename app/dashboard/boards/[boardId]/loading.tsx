fd7a70e70d84199db89da64e0696937cca030982
ni-aina <niaina.dev.soft@gmail.com>
2026-06-30 15:14:22 +0300

    Features/Assigned cards form

--- app/dashboard/boards/[boardId]/loading.tsx
+++ app/dashboard/boards/[boardId]/loading.tsx
@@ -4,7 +4,10 @@ import { Board } from "@prisma/client";
 
 const BoardLoading = () => (
     <div className="flex-1">
-        <BoardSpace board={{} as Board} />
+        <BoardSpace
+            members={[]}
+            board={{} as Board}
+        />
         <div className="px-4 lg:px-8 flex flex-wrap items-center gap-5">
             {
                 Array.from({ length: 3 }).map((_, index) => (
